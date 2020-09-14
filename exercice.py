f799ee2745b230f4475d0bc9225246aef4329f8f
Misterh-1 <hamidfagbemi079@gmail.com>
2020-09-14 22:26:56 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -15,7 +15,7 @@ def square(a: float) -> float:
 
 
 def average(a: float, b: float, c: float) -> float:
-    return a, b, c
+    return a, b, c, d
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
