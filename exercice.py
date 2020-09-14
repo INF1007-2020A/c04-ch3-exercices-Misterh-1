3d731725345aa3ef92b98bf404b96883adbbb296
Misterh-1 <hamidfagbemi079@gmail.com>
2020-09-14 22:52:13 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -23,6 +23,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
     degs_rad = (angle_degs * math.pi) / 180
     min_rad = (angle_mins * math.pi) / (60*180)
     secs_rad = (angle_secs * math.pi) / (3600*180)
+    result_rad = degs_rad + 
     return result_rad
 
 
