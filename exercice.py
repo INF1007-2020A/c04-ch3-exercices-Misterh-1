891915ebc270246066f45a777045368517a5bfa0
Misterh-1 <hamidfagbemi079@gmail.com>
2020-09-14 23:32:33 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -36,7 +36,7 @@ def to_degrees(angle_rads: float) -> tuple:
 
 def to_celsius(temperature: float) -> float:
     cels_temp = ((temperature - 32)*5)/9
-    return 0.0
+    return cels_temp
 
 
 def to_farenheit(temperature: float) -> float:
