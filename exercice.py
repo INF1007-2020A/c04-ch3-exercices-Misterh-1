90a01552042d69cc7472da1e0b87194f28c162c0
Misterh-1 <hamidfagbemi079@gmail.com>
2020-09-14 22:37:44 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -20,7 +20,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    degs_rad = (math.pi * angle_degs )
+    degs_rad = (math.pi * angle_degs )/2
     print(degs_rad)
 
     return 0.0
