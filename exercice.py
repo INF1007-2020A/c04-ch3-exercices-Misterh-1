8c4a4f511b3400e7a0010428cd570d43c5058a0c
Misterh-1 <hamidfagbemi079@gmail.com>
2020-09-14 22:55:16 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -28,7 +28,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    rad_deg = (60*180)
+    rad_deg = (60*180*angle_rads)
     return 0.0, 0.0, 0.0
 
 
