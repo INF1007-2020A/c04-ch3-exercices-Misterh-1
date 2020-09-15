a02472d482a051b1b9ec67fff3900722dfa0594b
Misterh-1 <hamidfagbemi079@gmail.com>
2020-09-15 00:15:27 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -37,7 +37,7 @@ def to_celsius(temperature: float) -> float:
 
 
 def to_farenheit(temperature: float) -> float:
-    fahr_temp = (temperature*) + 32
+    fahr_temp = (temperature*1.8) + 32
     return fahr_temp
 
 
